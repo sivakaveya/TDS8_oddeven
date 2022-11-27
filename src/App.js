cbc76f217697c829620c2044e8e0e8c015a06810
sivaruna10 <sivaaruna10@gmail.com>
2022-11-27 23:10:49 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+// import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      number: '',
+      odd_even:''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    
+  }
+
+
+  //Funxtion to handle the change in input and update the state
+  handleChange(event) {
+    const { name, value } = event.target
+    this.setState({ [name]: value })
+    // console.log(this.state.gender)
+  }
+
+  //function after the form is submitted
+  handleSubmit(event) {
+    alert(
+      'Number: ' + this.state.number 
+    );
+    event.preventDefault();
+    if (this.state.number==='' || isNaN(this.state.number)){
+      alert('Number is invalid!')
+    }
+    else{
+      let number=parseFloat(this.state.number);
+      if (number%2===0){
+        this.setState({odd_even:'Even'});
+      }
+      
+      else{
+        this.setState({odd_even:'Odd'});
+      }
+    }
+  }
+    
+
+  //function to increase the age
+  // handlePlus(e) {
+  //   e.preventDefault();
+  //   this.setState(prevState => {
+  //     return {
+  //       age: prevState.age + 1
+  //     }
+  //   })
+  // }
+
+  //funtcion to decrease the age
+  // handleMinus(e) {
+  //   e.preventDefault();
+  //   this.setState(prevState => {
+  //     return {
+  //       age: prevState.age - 1
+  //     }
+  //   })
+  //   console.log(this.state.age);
+  // }
+
+
+  render() {
+    return (
+      <div className='box'>
+        <form >
+          <h2>ODD EVEN
+          </h2>
+          <br />
+
+
+          <label>
+          <span style={{marginRight:10}}>Number: </span> 
+            <input
+              type="text"
+              className='inputt'
+              value={this.state.number}
+              name="number"
+              placeholder="Enter a number"
+              onChange={this.handleChange}
+            />
+          
+          </label>
+
+
+
+          <br />
+
+
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {this.state.odd_even===''?
+        ''
+        :'The entered Number is: '+this.state.odd_even+'.'
+        }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+
+    )
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
 
 export default App;
